6530d6b1e67b0ef5e1010d62c4c1a6fef84045c1
Tobias Genao <rtobiasgm@hotmail.com>
2019-07-08 06:17:33 -0400

    Api begin, seed database already

---
+++ Vega/Controllers/MakesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    public class MakesController : Controller
+    {
+
+        private readonly VegaDbContext context;
+
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(c => c.Models).ToListAsync();
+        }
+    }
+}
